76c091e43333e464619b7868c3d932674c185e5d
Justin Crawford <Justasic@Gmail.com>
2011-11-28 06:09:32 -0800

    Added files for the future bot class

---
+++ include/bot.h
@@ -0,0 +1,8 @@
+#ifndef _BOT_H
+#define _BOT_H
+#include "module.h"
+
+class CoreExport Bot
+{};
+
+#endif
--- include/network.h
+++ include/network.h
@@ -1,5 +1,14 @@
 #ifndef _NETWORK_H_
 #define _NETWORK_H_
+#include "module.h"
+#include "bots.h"
 
+class Network;
+struct CommitMessage
+{
+  std::vector<Flux::string> MessageMeta;
+  std::vector<Bot*> Bots;
+  Network *network;
+}
 
 #endif
