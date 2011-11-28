0baae91678c1d5d0e624e46761f16e0f30ba0016
Justin Crawford <Justasic@Gmail.com>
2011-11-28 15:04:32 -0800

    Added basics to Bot class

--- include/bot.h
+++ include/bot.h
@@ -3,6 +3,21 @@
 #include "module.h"
 
 class CoreExport Bot
-{};
+{
+public:
+  Bot(Network *net, const Flux::string &n, const Flux::string &i, const Flux::string &real): network(net), nick(n), ident(i), realname(real)
+  {
+  }
+  /* The network we're on*/
+  Network *network;
+  /* List of chans the bot is in */
+  Flux::map<Channel*> channels;
+  /* Bot's current nickname */
+  Flux::string nick;
+  /* Bot's current ident */
+  Flux::string ident;
+  /* Bot's current realname */
+  Flux::string realname;
+};
 
 #endif
