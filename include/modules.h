558ffe99620b5503159eb6bbc035bbf01faef6c6
Justin Crawford <Justasic@Gmail.com>
2012-01-08 21:07:52 -0800

    forgot another file -_-

---
+++ include/modules.h
@@ -0,0 +1,44 @@
+/* Arbitrary Navn Tool -- Main include for modules
+ * 
+ * (C) 2011-2012 Flux-Net
+ * Contact us at Dev@Flux-Net.net
+ *
+ * Please read COPYING and README for further details.
+ *
+ * Based on the original code of CIA.vc by Micah Dowty
+ * Based on the original code of Anope by The Anope Team.
+ */
+
+#ifndef MODULES_H
+#define MODULES_H
+#include "module.h"
+#include "INIReader.h"
+// #include "defs.h"
+#include "xmlfile.h"
+#include "network.h" //We'll solve includes later
+#include "bot.h"
+
+#ifdef HAVE_SETJMP_H
+jmp_buf sigbuf;
+#endif
+char **my_av, **my_envp;
+bool nofork, dev, protocoldebug, quitting, nocolor, istempnick = false;
+Flux::string binary_path, bot_bin, binary_dir, quitmsg, server_name, LastBuf;
+char segv_location[255];
+time_t starttime = 0;
+
+BotConfig *Config;
+Network *FluxNet;
+GlobalProto *GProto;
+module *LastRunModule; // For crashes
+
+E void startup(int argc, char** argv, char *envp[]);
+// E static void WritePID();
+E void Rehash();
+// E void restart(const Flux::string &reason);
+E Flux::string execute(const char *cmd);
+E Flux::string urlify(const Flux::string &received);
+E Flux::string getprogdir(const Flux::string &dir);
+
+
+#endif
