806babf7980fc8e499ecadd121520945ab9a7caa
justasic <justasic@gmail.com>
2012-05-02 17:38:37 +0000

    Use Colors.h instead of defining all this crap in here

--- modules/core/m_cia_rulesets.cpp
+++ modules/core/m_cia_rulesets.cpp
@@ -10,35 +10,7 @@
  */
 
 #include "modules.h"
-
-//IRC Colors
-#define BLACK "\0031"
-#define DARK_BLUE "\0032"
-#define DARK_GREEN "\0033"
-#define GREEN "\0033"
-#define RED "\0034"
-#define LIGHT_RED "\0034"
-#define DARK_RED "\0035"
-#define PURPLE "\0036"
-#define BROWN "\0037"
-#define ORANGE "\0037"
-#define YELLOW "\0038"
-#define LIGHT_GREEN "\0039"
-#define AQUA "\00310"
-#define LIGHT_BLUE "\00311"
-#define BLUE "\00312"
-#define VIOLET "\00313"
-#define GREY "\00314"
-#define GRAY "\00314"
-#define LIGHT_GREY "\00315"
-#define LIGHT_GRAY "\00315"
-#define WHITE "\00316"
-
-//Other formatting
-#define NORMAL "\17"
-#define BOLD "\2"
-#define REVERSE ""
-#define UNDERLINE "\13"
+#include "colors.h"
 
 // TODO: This needs to parse CIA style rulesets and then do a FOREACH_MOD(I_OnCommit, OnCommit) call
 // to announce the messages in all of IRC and other systems. This should also be one of the main calls
