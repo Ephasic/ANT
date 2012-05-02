5898de6599ab8a83ec262cef79b7094bb5e61dd7
justasic <justasic@gmail.com>
2012-05-02 17:37:46 +0000

    Added Colors.h

---
+++ include/colors.h
@@ -0,0 +1,44 @@
+/* Arbitrary Navn Tool -- Color codes for IRC and HTML
+ * (C) 2011-2012 Azuru
+ * Contact us at Development@Azuru.net
+ *
+ * Please read COPYING and README for further details.
+ *
+ * Based on the original code of CIA.vc by Micah Dowty
+ * Based on the original code of Anope by The Anope Team.
+ */
+
+#pragma once
+#ifndef COLORS_H
+#define COLORS_H
+
+//IRC Colors
+#define BLACK "\0031"
+#define DARK_BLUE "\0032"
+#define DARK_GREEN "\0033"
+#define GREEN "\0033"
+#define RED "\0034"
+#define LIGHT_RED "\0034"
+#define DARK_RED "\0035"
+#define PURPLE "\0036"
+#define BROWN "\0037"
+#define ORANGE "\0037"
+#define YELLOW "\0038"
+#define LIGHT_GREEN "\0039"
+#define AQUA "\00310"
+#define LIGHT_BLUE "\00311"
+#define BLUE "\00312"
+#define VIOLET "\00313"
+#define GREY "\00314"
+#define GRAY "\00314"
+#define LIGHT_GREY "\00315"
+#define LIGHT_GRAY "\00315"
+#define WHITE "\00316"
+
+//Other IRC formatting
+#define NORMAL "\17"
+#define BOLD "\2"
+#define REVERSE ""
+#define UNDERLINE "\13"
+
+#endif
