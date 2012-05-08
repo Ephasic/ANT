a7c436e230ecefb2f63079b6b4ff03896f20dd43
justasic <justasic@gmail.com>
2012-05-08 16:56:28 +0000

    Use the traditional iteration method instead of the C++11 method as we may have an invalid iterator.

--- src/Sepstream.cpp
+++ src/Sepstream.cpp
@@ -126,8 +126,8 @@ Base::Base() { Log(LOG_MEMORY) << "Classbase::+ @" << this; }
 Base::~Base()
 {
   Log(LOG_MEMORY) << "Classbase::- @" << this;
-  for(auto it : this->References)
-    it->Invalidate();
+  for(std::set<dynamic_reference_base*>::iterator it = this->References.begin(), it_end = this->References.end(); it != it_end; ++it)
+    (*it)->Invalidate();
 }
 void Base::AddReference(dynamic_reference_base *r) { this->References.insert(r); }
 void Base::DelReference(dynamic_reference_base *r) { this->References.erase(r); }
